929b0c41bfdca278cd65162350aa7a3200677977
Patrick McGreevy <patrick.mcgreevy99@gmail.com>
2024-05-11 16:01:58 -0700

    Fixes status codes
    Needed to add a -linecomment flag to make the custom names work.

--- pkg/libhttp/status_code.go
+++ pkg/libhttp/status_code.go
@@ -2,8 +2,8 @@ package libhttp
 
 type StatusCode int
 
-//go:generate stringer -type=StatusCode
+//go:generate stringer -type=StatusCode  -linecomment
 const (
-	OK       StatusCode = 201 // OK
+	OK       StatusCode = 200 // OK
 	NotFound StatusCode = 404 // Not Found
 )
